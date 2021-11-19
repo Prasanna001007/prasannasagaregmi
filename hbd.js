a0fa130eba45f91a397b8edffa8d0e5ed421c887
Prasanna001007 <94697175+Prasanna001007@users.noreply.github.com>
2021-11-19 20:37:55 +0545

    Add files via upload
    website for ...........

---
+++ hbd.js
@@ -0,0 +1,24 @@
+var button=document.getElementById("btn-1");
+
+button.addEventListener("click",function(){
+this.innerHTML="Happy";
+this.style="padding:10px;font-size:40px;";
+
+})
+
+var button2=document.getElementById("btn-2");
+
+button2.addEventListener("click",function(){
+this.innerHTML="Birthday";
+this.style="padding:10px;font-size:40px;";
+
+
+})
+
+var button3=document.getElementById("btn-3");
+
+button3.addEventListener("click",function(){
+this.innerHTML="Dost";
+this.style="padding:10px;font-size:40px;";
+
+})
